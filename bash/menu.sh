5b38dfefb28603719c7292b0876e0fa0d8116082
lollo11123 <161380557+lollo11123@users.noreply.github.com>
2024-05-13 11:07:57 +0000

    primo

---
+++ bash/menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+while ((1))
+do
+    clear
+    echo "digita 1 per..."
+    echo "digita 2 per..."
+    read scelta
+    if ((scelta==0))
+    then
+        exit
+    fi
+done 
+
